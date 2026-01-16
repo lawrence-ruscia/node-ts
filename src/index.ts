b4055093187c28ff5c3fd1d894f7884c2860c678
lawrence-ruscia <140231442+lawrence-ruscia@users.noreply.github.com>
2026-01-17 06:50:11 +0800

    Add error-handling middleware

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,33 @@
-import express from 'express'
-import 'dotenv/config'
-import path from 'node:path'
-import { rootRouter } from './routers/root.router.js'
+import express from 'express';
+import 'dotenv/config';
+import path from 'node:path';
+import { rootRouter } from './routers/root.router.js';
 
-const app = express()
-const PORT = process.env.PORT ?? 3000
-const dirname = import.meta.dirname
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+const dirname = import.meta.dirname;
 
 // App-level Middleware
-app.use(express.urlencoded())
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // App Config
-app.set('views', path.join(dirname, 'views'))
-app.set('view engine', 'ejs')
+app.set('views', path.join(dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // Routers
-app.use('/', rootRouter)
+app.use('/', rootRouter);
+
+// Error Handler Middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
 
 app.listen(PORT, (err) => {
   if (err) {
-    throw err
+    throw err;
   }
 
-  console.log(`Server running on port ${PORT.toString()}`)
-})
+  console.log(`Server running on port ${PORT.toString()}`);
+});
